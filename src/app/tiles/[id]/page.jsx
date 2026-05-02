2ad2984f7e189cb10c56134f542e6dd302a840f9
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-05-02 12:30:47 +0600

    json id re-named

--- src/app/tiles/[id]/page.jsx
+++ src/app/tiles/[id]/page.jsx
@@ -1,12 +1,23 @@
+import { getTiles } from "@/lib/getTiles";
 
-const TilesDetails = async ({params}) => {
-    const {id}=await params
-    console.log(id);
-    return (
-        <div>
-            tailes details page
-        </div>
-    );
+const TilesDetails = async ({ params }) => {
+  const id = params?.id;
+
+  const details = await getTiles();
+
+  const tile = details.find((t) => t.id === id);
+
+  if (!tile) {
+    return <div>Tile not found</div>;
+  }
+
+  return (
+    <div>
+      <h1>{tile.title}</h1>
+      <p>{tile.description}</p>
+      <img src={tile.image} alt={tile.title} />
+    </div>
+  );
 };
 
 export default TilesDetails;
